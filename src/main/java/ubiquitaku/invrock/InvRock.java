8b07c9eb66b8ba5c62ada63475da92a69f323903
ubiquitaku <ubiquitaku@outlook.jp>
2020-07-18 19:11:41 +0900

    (0言0́*)<ヴェアアアアアアアア

---
+++ src/main/java/ubiquitaku/invrock/InvRock.java
@@ -0,0 +1,62 @@
+package ubiquitaku.invrock;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InvRock extends JavaPlugin {
+    boolean r;
+    FileConfiguration config;
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        try {
+            r = config.getBoolean("r");
+        } catch (NumberFormatException kaku){
+            r = false;
+            config.set("r",false);
+            System.out.println("configの値を取得できなかったためデフォルトのfalseに設定されました");
+        }
+        r = config.getBoolean("r",false);
+        saveConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equals("ir")) {
+            if (sender.isOp()) {
+                if (args[0].equals("on")) {
+                    r = true;
+                    return true;
+                }
+                if (args[0].equals("off")) {
+                    r = false;
+                    return true;
+                }
+            }
+        }
+        return true;
+    }
+
+    @EventHandler
+    public void rock(InventoryClickEvent e) {
+        Player p = (Player) e.getWhoClicked();
+        if (p.isOp()) {
+            return;
+        }
+        e.setCancelled(true);
+        e.setCancelled(false);
+        return;
+    }
+}
